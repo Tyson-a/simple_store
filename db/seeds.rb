110468610978d21a42b5c33e55b745913dc47c59
Tyson La <tysonala@hotmail.com>
2024-02-08 00:57:58 -0600

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
 # db/seeds.rb
 require "csv"
 
-
 # Load data from products.csv file
 CSV.foreach(Rails.root.join("db/products.csv"), headers: true) do |row|
   category = Category.find_or_create_by(name: row["category"])
-  product = category.products.create(
+
+  category.products.create(
     title: row["title"],
     description: row["description"],
     price: row["price"],
     stock_quantity: row["stock_quantity"]
   )
 end
+
+
