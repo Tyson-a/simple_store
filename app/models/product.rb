27c87140217fe171b31e20cd04b9d28b866d0cd7
Tyson La <tysonala@hotmail.com>
2024-02-08 00:18:06 -0600

    Added associations to Category and Product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  belongs_to :category
   validates :title, presence: true
   validates :price, presence: true, numericality: true
   validates :stock_quantity, presence: true, numericality: { only_integer: true }
